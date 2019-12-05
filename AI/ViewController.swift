ceeea87a766b3618291fcda5b716dc5852927633
pastre <pastre68@gmail.com>
2019-12-05 09:24:07 -0300

    Initial Commit

---
+++ AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AI
+//
+//  Created by Bruno Pastre on 05/12/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
